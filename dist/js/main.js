27b3c26d4684366bed6aaea17f352d12f370f95d
Jose <chaparro.jjr@gmail.com>
2020-12-09 18:56:32 -0700

    contact form html and css complete. Added media queries to the hero section. JS function set for opening and closing navbar

---
+++ dist/js/main.js
@@ -0,0 +1,18 @@
+const bodyElement = document.querySelector('body');
+const menuToggle = document.querySelector('.hamburger-menu');
+const heroElement = document.querySelector('.hero');
+
+// Toggle mobile nav
+menuToggle.addEventListener('click', function () {
+  bodyElement.classList.toggle('is-open');
+  heroElement.classList.add('is-open');
+});
+
+window.addEventListener('click', function (e) {
+  let clickedElement = e.target;
+
+  if (clickedElement.matches('.is-open')) {
+    bodyElement.classList.remove('is-open');
+    heroElement.classList.remove('is-open');
+  }
+});
